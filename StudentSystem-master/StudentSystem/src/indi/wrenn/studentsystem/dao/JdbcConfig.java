d62e9409d21df79da2875178ca198d7305571ae1
Macr646 <2403800835@qq.com>
2025-05-21 21:55:59 +0800

    Delete StudentSystem-master directory

--- StudentSystem-master/StudentSystem/src/indi/wrenn/studentsystem/dao/JdbcConfig.java
+++
@@ -1,8 +0,0 @@
-package indi.wrenn.studentsystem.dao;
-
-public interface JdbcConfig {
-    String DRIVER = "com.mysql.jdbc.Driver";
-    String URL = "jdbc:mysql://localhost:3306/StudentSystemDao";
-    String USERNAME = "root";
-    String PASSWORD = "";
-}
